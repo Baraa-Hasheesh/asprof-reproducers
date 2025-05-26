ec8007153d0e13c180cade9854c387ec6c362aff
Bara' Hasheesh <bara.hasheesh@gmail.com>
2025-05-26 09:52:56 +0000

    include local so & automate convert of output format

--- doubleprofilers/doublectimer/runner.sh
+++ doubleprofilers/doublectimer/runner.sh
@@ -5,14 +5,23 @@ if ! [[ $1 == "single" ]] && ! [[ $1 == "multi" ]]; then
     exit 1
 fi
 
-java -agentpath:$(pwd)/libLocal.so=start,event=ctimer,signal=40,file=output.jfr -Djava.library.path="$(pwd)" DoubleProfiler.java DoubleProfiler &
+MODE=$1
+
+java -agentpath:$(pwd)/libLocal.so=start,event=ctimer,signal=40,file=output_${MODE}.jfr -Djava.library.path="$(pwd)" DoubleProfiler.java DoubleProfiler &
 
 JAVA_PID=$!
 
-if [ $1 == "multi" ]; then
+if [ $MODE == "multi" ]; then
     sleep 1
     ASPROF=$(realpath "${PATH_TO_ASYNC_PROFILER_LIB_DIR}/../bin/asprof")
     $ASPROF start -e ctimer --signal 41 -f output_attach.jfr ${JAVA_PID}
 fi
 
 wait $JAVA_PID
+
+JFRCONV=$(realpath "${PATH_TO_ASYNC_PROFILER_LIB_DIR}/../bin/jfrconv")
+$JFRCONV output_${MODE}.jfr output_${MODE}.html
+
+if [ $MODE == "multi" ]; then
+    $JFRCONV output_attach.jfr output_attach.html
+fi
